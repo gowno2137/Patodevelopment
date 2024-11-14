971e04967acb27111b27b399a431b17e9fcc83ac
Antexxxxx <antek.wrobel.pl@gmail.com>
2024-11-14 22:00:50 +0100

    nav

--- index.php
+++ index.php
@@ -7,6 +7,6 @@
 </head>
 <body>
     <header><h1>Patodevelopment</h1></header>
-    
+    <nav><a href="index.php">Strona główna</a><a href="index.php">Loteria</a><a href="index.php">Kontakt</a><a href="index.php">Opinie</a></nav>
 </body>
 </html>
