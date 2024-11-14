3ba3bd1610257184efcd769694f80c1cc181d0c9
Szymon <beniocek1984@gmail.com>
2024-11-14 22:03:12 +0100

    c

--- index.php
+++ index.php
@@ -7,6 +7,6 @@
 </head>
 <body>
     <header><h1>Patodevelopment</h1></header>
-    <nav><a href="index.php">Strona główna</a><a href="index.php">Loteria</a><a href="index.php">Kontakt</a><a href="index.php">Opinie</a></nav>
+    <nav><a href="index.php">Strona główna</a><a href="index.php">Loteria</a><a href="index.php">Kontakt</a><a href="index.php">Opinie</a><a href="index.php">Kup mieszkanie</a></nav>
 </body>
 </html>
