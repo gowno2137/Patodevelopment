edc77ba31520e25eef880de470a7732261b3b7d4
Szymon <beniocek1984@gmail.com>
2024-11-14 22:23:50 +0100

    c

---
+++ kontakt.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>kontakt</title>
+</head>
+<body>
+    <h1>Dane do pozwu</h1>
+    <p>Nazwa firmy: Patodevelopment</p>
+    <p>Adres: ul. ŚWIĘTEJ ELŻBIETY 1 41-905 BYTOM</p>
+    <p>Zarząd: Partyk, Anthony, Żydmon</p>
+    <p>NIP: 6263045919</p>
+    <p>Regon: 520679377</p>
+    <a href="mailto: patodevelopment@hazard.com">patodevelopment@gowno.com</a><br>
+    <a href="tel:+48213769420">+48 213 769 420</a>
+</body>
+</html>
