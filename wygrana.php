4d9b4a66632a45a8bb9878bffe60d4bc36babc62
Antexxxxx <antek.wrobel.pl@gmail.com>
2024-11-15 19:10:56 +0100

    Create wygrana.php

---
+++ wygrana.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wygrana</title>
+</head>
+<body>
+    <h1 style="font: size 400px;">MEGA BIG WIN</h1>
+</body>
+</html>
