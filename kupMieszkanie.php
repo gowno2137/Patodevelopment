a9653aa17a542bcd6db8801aa48bf4ccb57a73e8
Antexxxxx <antek.wrobel.pl@gmail.com>
2024-11-19 22:45:01 +0100

    Update kupMieszkanie.php

--- kupMieszkanie.php
+++ kupMieszkanie.php
@@ -40,5 +40,7 @@
     </nav>
     <h1>Kup Mieszkanie!</h1>
     <h2>Tanio! Dogodne lokalizacje</h2>
+    <h1>kiedy cena jest za mała groborzowi staje pała</h1>
+    <h2>groborz to cimny goźć</h2>
 </body>
 </html>
