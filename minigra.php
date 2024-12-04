c7f581c002d43aff2d9d318b4d7d9542602d6988
Antexxxxx <antek.wrobel.pl@gmail.com>
2024-12-04 22:38:02 +0100

    3w

---
+++ minigra.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="minigra.js"></script>
+    <title>Minigra</title>
+</head>
+<body>
+    <button>Start Animation</button>
+
+    <div style="background:#98bf21;height:100px;width:100px;position:absolute;"></div>
+</body>
+</html>
