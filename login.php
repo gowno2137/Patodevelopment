29a34113e3c0208c2f1094df3549fca4c2d84cd1
Szymon <beniocek1984@gmail.com>
2024-12-05 23:18:15 +0100

    Update login.php

--- login.php
+++ login.php
@@ -26,27 +26,24 @@
 
     <form class="row g-3">
       <div class="col-md-4">
-        <label for="validationDefault01" class="form-label">email</label>
-        <input type="text" class="form-control" id="validationDefault01" required="">
+        <label for="validationDefault01" name="email" class="form-label">email</label>
+        <input type="text" class="form-control" id="email" required>
       </div>
       <div class="col-md-4">
-        <label for="validationDefault02" class="form-label">hasło</label>
-        <input type="text" class="form-control" id="validationDefault02" required="">
+        <label for="validationDefault02" naem="haslo" class="form-label">hasło</label>
+        <input type="password" class="form-control" id="haslo" required>
       </div>
       <div class="col-12">
         <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="invalidCheck2" required="">
+          <input class="form-check-input" type="checkbox" value="" id="invalidCheck2" required>
           <label class="form-check-label" for="invalidCheck2">
             Akceptuję regulamin firmy patodevelopment
           </label>
         </div>
       </div>
       <div class="col-12">
-        <button class="btn btn-primary" type="submit">Submit form</button>
+        <button class="btn btn-primary" type="submit">Zaloguj się</button>
       </div>
     </form>
-    <script>
-
-    </script>
 </body>
 </html>
