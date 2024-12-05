a4db00051bb120e4d258117ad3f3e705d599e66c
Szymon <beniocek1984@gmail.com>
2024-12-05 22:21:55 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
 </head>
 <body>
-    
+    <form action="" method="post">
+        <h2>Zaloguj się</h2>
+        <label for="email">email: <input type="email" name="email" placeholder="email"></label>
+        
+        <label for="haslo">hasło: <input type="password" name="haslo" placeholder="hasło"></label>
+        <input type="submit" value="Zaloguj">
+    </form>
 </body>
 </html>
