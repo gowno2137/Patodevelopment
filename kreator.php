41c59e3fcff8af45c24da13f4c85ba939144a9c0
Szymon <beniocek1984@gmail.com>
2024-12-06 00:00:44 +0100

    Update kreator.php

--- kreator.php
+++ kreator.php
@@ -1,4 +1,23 @@
-<?php
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body{
+            background-image: url(l5.jpg);
+            background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  height: 100vh;
+  overflow:hidden;
+        }
+    </style>
+</head>
+<body>
+
+    <?php
     session_start();
     
     if (!isset($_SESSION["email"])) {
@@ -7,3 +26,5 @@
         exit();
     }
 ?>
+</body>
+</html>
